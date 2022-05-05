1a7d71503b3b624d481af3b621b595a6657b1dcc
Tommy Li <tommyli76278@gmail.com>
2022-05-05 15:26:07 -0700

    set up card api and card style

--- main.js
+++ main.js
@@ -1,42 +1,34 @@
 // https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
 
+let deckID = '';
+
+const makeNewDeck = async () => {
+    try {
+        const value = document.querySelector('input').value;
+        const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${value}`;
+        const res = await axios.get(url);
+        deckID = res.data.deck_id;
+        document.querySelector('#deckID').textContent += deckID;
+        document.querySelector('#cardCount').textContent += res.data.remaining;
+    } catch (e) {
+        console.log(`ERROR: ${e}`);
+    }
+}
+
+const drawCard = async () => {
+    try {
+        const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`;
+        const res = await axios.get(url);
+        const currentCard = res.data.cards[0];
+        console.log(currentCard);
+    } catch (e) {
+        console.log(`ERROR: ${e}`);
+    }
+}
+
 const newDeck = document.querySelector('#create');
 newDeck.addEventListener('click', makeNewDeck)
 
-let deckID = '';
-
-function makeNewDeck() {
-    const value = document.querySelector('input').value;
-    const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${value}`;
-    getFetch(url);
-}
-
-function getFetch(url) {
-    fetch(url)
-        .then(res => res.json())//parse response as JSON
-        .then(data => {
-            deckID = data.deck_id;
-            document.querySelector('#deckID').textContent += deckID;
-            document.querySelector('#cardCount').textContent += data.remaining;
-        })
-        .catch(err => {
-            console.log(`error ${err}`);
-        })
-}
-
-const drawCard = () => {
-    const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`;
-    fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(err => {
-            console.log(`error ${err}`);
-        })
-}
-
 const hit = document.querySelector('#hit');
 hit.addEventListener('click', drawCard);
+
