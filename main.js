b768af93af45b64a10ff2616fc447af2f9c5f99b
Tommy Li <tommyli76278@gmail.com>
2022-05-05 17:38:05 -0700

    polished javascript card creatioin from api

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 // https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+const deckID = document.querySelector('#deckID span');
+const cardCount = document.querySelector('#cardCount span');
+const newCard = document.querySelector('#currentCard span');
+const cardTemplate = document.querySelector('#template');
+console.log(suits);
 
-let deckID = '';
+let id = '';
 
 const makeNewDeck = async () => {
     try {
         const value = document.querySelector('input').value;
         const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${value}`;
         const res = await axios.get(url);
-        deckID = res.data.deck_id;
-        document.querySelector('#deckID').textContent += deckID;
-        document.querySelector('#cardCount').textContent += res.data.remaining;
+        id = res.data.deck_id;
+        deckID.textContent = id;
+        cardCount.textContent = res.data.remaining;
     } catch (e) {
         console.log(`ERROR: ${e}`);
     }
@@ -17,13 +22,24 @@ const makeNewDeck = async () => {
 
 const drawCard = async () => {
     try {
-        const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`;
+        const url = `https://deckofcardsapi.com/api/deck/${id}/draw/?count=1`;
         const res = await axios.get(url);
         const currentCard = res.data.cards[0];
-        console.log(currentCard);
+        const numberSuit = currentCard.value + ' Of ' + currentCard.suit;
+        newCard.textContent = numberSuit;
+        cardCount.textContent = res.data.remaining;
+        addCard(currentCard);
     } catch (e) {
         console.log(`ERROR: ${e}`);
     }
+}
+
+const addCard = (card) => {
+    let clone = cardTemplate.cloneNode(true);
+    clone.style.display = "inline-block";
+    clone.querySelector('h4') = card.value;
+    clone.querySelector()
+    document.body.append(clone);
 }
 
 const newDeck = document.querySelector('#create');
@@ -31,4 +47,3 @@ newDeck.addEventListener('click', makeNewDeck)
 
 const hit = document.querySelector('#hit');
 hit.addEventListener('click', drawCard);
-
